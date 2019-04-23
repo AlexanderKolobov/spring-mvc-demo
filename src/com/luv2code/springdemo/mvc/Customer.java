27303a7e768b6344303c0267d2ba96af5bd5458e
Oleksandr Kolobov <okolobov@exadel.com>
2019-04-23 13:00:35 +0300

    Spring MVC Form Validation - Validating Number Ranges

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,5 +1,7 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -10,6 +12,10 @@ public class Customer
     @NotNull( message = "is required" )
     @Size(min = 1, message = "is required")
     private String lastName;
+
+    @Min( value = 1, message = "must be greater then or equal to one")
+    @Max( value = 10, message = "must be less then or equal to ten")
+    private Integer freePasses;
 
     public String getFirstName()
     {
@@ -29,5 +35,15 @@ public class Customer
     public void setLastName( String lastName )
     {
         this.lastName = lastName;
+    }
+
+    public Integer getFreePasses()
+    {
+        return freePasses;
+    }
+
+    public void setFreePasses( Integer freePasses )
+    {
+        this.freePasses = freePasses;
     }
 }
